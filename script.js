17fcce4a0ceb063901861a59c66b7b87e1d28aa7
Mnats Avetisyan <avetisian.exceedteam@gmail.com>
2022-07-15 09:10:39 +0300

    fix/comments

--- script.js
+++ script.js
@@ -1,91 +1,84 @@
-let allTasks = JSON.parse(localStorage.getItem("tasks")) || []; // то где мы храним таски
+let allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let input = ""; // глобальное пустое значение
-let valueInput = ""; // глобальное пустое значение
-let renameValue = ""; // глобальное пустое значение
+let input = "";
+let valueInput = "";
+let renameValue = "";
 
 window.onload = function init() {
   input = document.getElementById("text_input");
   input.addEventListener("change", updateValue);
   render();
-  // localStorage.setItem("tasks", JSON.stringify(allTasks));
 };
 
 onClickButton = () => {
-  //при нажатии на кнопку
   if (valueInput !== "" && valueInput !== " ") {
     allTasks.push({
-      text: valueInput, //значение каких обьектов будет пушиться в массив
+      text: valueInput,
       isCheck: false,
       isEdit: false,
     });
   }
   localStorage.setItem("tasks", JSON.stringify(allTasks));
-  valueInput = ""; //обнуление глобальных переменных
-  input.value = ""; //обнуление глобальных переменных
-  render(); // функция отображения всех элементов
+  valueInput = "";
+  input.value = "";
+  render();
 };
 
 updateValue = (event) => {
-  valueInput = event.target.value; //
+  valueInput = event.target.value;
   renameValue = event.target.value;
 };
 
-//функция которая отрисовывает массив alltasks
 render = () => {
-  const content = document.getElementById("tasks_list"); // присваеиваем content наш блок с айди task-list
+  const content = document.getElementById("tasks_list");
   while (content.firstChild) {
-    //удаление первых дочерних элементов
-    content.removeChild(content.firstChild); // до момента пока не остаётся не один дочерний элемент
+    content.removeChild(content.firstChild);
   }
 
-  allTasks.sort((a, b) =>
-    a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0
-  );
+  allTasks.sort((a, b) => a.isCheck - b.isCheck);
 
   allTasks.map((item, index) => {
-    const container = document.createElement("div"); //создаём контейнер для нашего task
+    const container = document.createElement("div");
 
-    const editForm = document.createElement("input"); //создаём инпут при изменении нашего значения isEdit
-    editForm.value = item.text; //исходное значение нашего editForm
-    editForm.addEventListener("change", updateValue); //слушатель на изменение
+    const editForm = document.createElement("input");
+    editForm.value = item.text;
+    editForm.addEventListener("change", updateValue);
 
-    container.id = `task=${index}`; //интедификатор нашего контейнера
-    const checkbox = document.createElement("input"); //инпут типа чекбокс
-    const text = document.createElement("p"); // создаём текст для наших тасок
+    container.id = `task=${index}`;
+    const checkbox = document.createElement("input");
+    const text = document.createElement("p");
 
-    text.className = item.isCheck ? "text-task done-text" : "text-task"; //если его задача выполнена мы добавляем стиль, а иначе нет
-    container.className = "task-container"; //создаём класс для нашего контейнера
+    text.className = item.isCheck ? "text-task done-text" : "text-task";
+    container.className = "task-container";
 
-    text.innerText = item.text; //добавляем название нашей задачи
-    checkbox.type = "checkbox"; // тип инпута
-    checkbox.checked = item.isCheck; // дефолтное свойство нашего чекбокса(false)
+    text.innerText = item.text;
+    checkbox.type = "checkbox";
+    checkbox.checked = item.isCheck;
 
-    const imageEdit = document.createElement("img"); //создаём элемент с тега img
+    const imageEdit = document.createElement("img");
     imageEdit.src = "img/edit.svg";
     imageEdit.onclick = () => {
       onEdit(index);
     };
 
-    const imageRename = document.createElement("img"); //создаём элемент с тега img
+    const imageRename = document.createElement("img");
     imageRename.src = "img/done.svg";
     imageRename.onclick = () => {
       onRename(index);
     };
 
-    const imageDelete = document.createElement("img"); //создаём элемени с тега img
+    const imageDelete = document.createElement("img");
     imageDelete.src = "img/delete.svg";
     imageDelete.onclick = () => {
       onDelete(index);
     };
 
-    content.appendChild(container); // добавляем контейнер, в наш блок "task-list"
-    container.appendChild(checkbox); // добавляем наш созданный инпут типа "чекбокс", в наш контейнер
-    container.appendChild(text); // добавляем наш текст в контейнер
-    container.appendChild(imageEdit); // добавляем к контейнеру наш imageEdit
-    container.appendChild(imageDelete); // добавляем к контейнеру наш imageDelete
+    content.appendChild(container);
+    container.appendChild(checkbox);
+    container.appendChild(text);
+    container.appendChild(imageEdit);
+    container.appendChild(imageDelete);
 
-    // изменение внешнего вида в зависимости от значения isEdit
     if (allTasks[index].isEdit) {
       container.appendChild(editForm);
       container.removeChild(checkbox);
@@ -95,7 +88,7 @@ render = () => {
 
     content.appendChild(container);
 
-    checkbox.onchange = function () {
+    checkbox.onchange = () => {
       onChangeCheckBox(index);
     };
   });
